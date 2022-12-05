1df4dad8ea43a647bbfefd542d04b9f3401ebc97
JoniArriazu <joni.arriazu2@gmail.com>
2022-12-05 13:03:18 -0300

    muestra-nombre and pagina1 components were added to the project

---
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina1',
+  templateUrl: './pagina1.component.html'
+})
+export class Pagina1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
