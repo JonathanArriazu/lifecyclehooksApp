7841b32fe584a51d65c16d3d624f4c470d2a22cb
JoniArriazu <joni.arriazu2@gmail.com>
2022-12-06 11:44:19 -0300

    Some exercisses about lifecycle hooks were performed to understand more about the topic

--- src/app/components/muestra-nombre/muestra-nombre.component.ts
+++ src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-muestra-nombre',
   templateUrl: './muestra-nombre.component.html'
 })
-export class MuestraNombreComponent implements OnInit {
+export class MuestraNombreComponent implements OnInit, OnChanges {
+
+  @Input() nombre!: string; //!: le dice a Angular que siempre va a tener un valor
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
 
   ngOnInit(): void {
   }
